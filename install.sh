6ee066bd734c1ad969c6565b92d05dca906867ad
Niclas <c14nnm@cs.umu.se>
2019-01-02 23:29:08 +0000

    Install library script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+sudo apt-get update -y 
+sudo apt-get install xterm
+
+sudo apt-get install git
+
+sudo apt-get install python
+sudo apt-get install python-pip
+pip install Flask
+sudo apt-get install nginx
